5a3e6ce6cfb40e581058f053abb69f29b233a5bb
Skezza <skezzashredder@gmail.com>
2025-09-27 12:25:01 -0700

    Update timer.h

--- kernel/timer.h
+++ kernel/timer.h
@@ -1,10 +1,12 @@
 #ifndef TIMER_H
 #define TIMER_H
+
 #include <stdint.h>
 
-/* Initialise the Programmable Interval Timer (PIT) to interrupt at
- * the given frequency (Hz).  Only the hardware is programmed; you
- * still need to install an IRQ handler to receive timer ticks. */
+/* Global tick count incremented by the PIT interrupt handler */
+extern volatile uint64_t timer_ticks;
+
+/* Initialise the Programmable Interval Timer (PIT) to interrupt at the given frequency (Hz). */
 void timer_init(uint32_t freq);
 
 #endif /* TIMER_H */
